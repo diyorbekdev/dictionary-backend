c5c0c61e4a3bd4fceb2d1d7f869ddb021c22976d
Developer <diyorbektoraboyev782@gmail.com>
2023-08-21 18:51:47 +0500

    new project

---
+++ src/test/java/com/diyorbek/dictionarybackend/DictionaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diyorbek.dictionarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
